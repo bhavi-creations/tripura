0ccd92e6de4026a91a6273bd7c9a7b53c52a8b51
yuvanandini <dharmadanandini@gmail.com>
2024-04-28 07:49:05 +0530

    all changes are updated images added

--- galleryvideos.php
+++ galleryvideos.php
@@ -29,13 +29,12 @@
 
     <script src="https://kit.fontawesome.com/742df65007.js" crossorigin="anonymous"></script>
 
-
-    <!-- For Modal links  -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+<!-- For Modal links  -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
-    crossorigin="anonymous"></script>
+integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
+crossorigin="anonymous"></script>
 
 </head>
 
@@ -100,106 +99,28 @@
         </div>
     </section>
 
-    <div class="section1 mt-5">
-        <div class="container">
-            <div class="row">
-                <div class="col-12">
-                    <h1 class="blogs-head" style="font-size: 42px;">Pigmentation</h1>
-                    <div class="d-flex flex-row justify-content-center">
-                        <svg  xmlns="http://www.w3.org/2000/svg" width="326" height="13" viewBox="0 0 326 13" fill="none">
-                            <path fill-rule="evenodd" clip-rule="evenodd" d="M7.49402 10.368C14.4034 9.59066 32.5617 9.55179 81.8547 9.47405C158.827 9.35744 219.917 8.50234 236.895 7.25853C238.243 7.14193 130.347 6.28681 119.267 6.90871C101.993 7.91931 19.5012 7.06419 2.90174 5.89812C-1.81689 5.58717 -0.0473825 4.49884 3.02815 4.26562C7.83102 3.87694 20.7231 3.91581 56.0707 3.99355C80.9278 4.03242 278.689 3.13843 300.302 1.5448C312.435 0.65082 319.808 -0.165424 323.179 0.0289192C326.001 0.184395 326.044 0.65082 323.6 1.93349C321.957 2.78861 323.811 3.29391 324.99 3.52712C326.802 3.91581 326.043 5.23735 323.768 6.28681C320.693 7.72496 315.384 7.68609 317.912 6.24794C318.46 5.93699 318.586 5.15961 316.648 5.31509C311.003 5.78152 271.105 8.96876 260.278 9.51292C144.671 15.421 31.8455 11.1066 7.95741 12.5447C-0.510838 13.0889 3.36523 10.8345 7.49402 10.368Z" fill="#25B4F8"/>
-                          </svg>
-                    </div>
-                    <p class="blogs_para alignments mt-2">Achieve clearer, more even-toned skin with our pigmentation treatments, targeting hyperpigmentation and melasma for a radiant complexion.</p>
-                </div>
-            </div>
+   <div class="container">
+    <div class="row">
+        <div class="col-12 mt-5">
+            <h1 class="text-center">Gallery</h1>
         </div>
+        <div class="d-flex flex-row justify-content-center">
+        <div class="col-3">
+            <a href="gallery.php"><button class="doc_button" class="btn btn-primary" >Images</button></a>
+        </div>
+        <div class="col-2">
+            <a href="galleryvideos.php"><button class="doc_button" class="btn btn-primary" >Videos</button></a>
+        </div>
+        </div>
+       
     </div>
+   </div>
 
-    <!-- Pigmentation -->
-    <section id="Pigmentation">
-        <div class="container mt-5  div_first_con">
-            <div class="row">
-                <div class="col-md-6  d-flex flex-column justify-content-center mt-2">
-                    <img src="assets/images/pigmentation lar.png" class=" doc_img_one img-fluid" alt="...">
+   <section style="height:200px; width:200px">
 
-                </div>
-                <div class="col-md-6 ">
-                    <h6 class="shedual_heading">Make a Schedule</h6>
-                    <h2 class="shedual_para">Pigmentation</h2>
-                    <p class="sahde_color">Experience the transformative effects of Pigmentation Reduction Peels at our clinic, where specialized peels are tailored to effectively reduce pigmentation and reveal a more even skin tone. These peels contain potent ingredients that target melanin production, break down existing pigmentation, and promote skin renewal. By exfoliating the skin and stimulating cell turnover, pigmentation reduction peels help fade dark spots, sun damage, and uneven skin tone, leaving you with a more radiant and uniform complexion. Unveil brighter, smoother skin with our customized pigmentation reduction peel treatments.</p>
-                    <div class="btn_flex_center">
-                        <button class="schedual_button" data-bs-toggle="modal" data-bs-target="#exampleModal"> Schedule Now!</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-
-    <!-- Reduction Peels -->
-    <section id="Reduction_Peels">
-        <div class="container mt-5  div_first_con">
-            <div class="row">
-                
-                <div class="col-md-6 order-2 order-md-1">
-                    <h6 class="shedual_heading">Make a Schedule</h6>
-                    <h2 class="shedual_para">Reduction Peels</h2>
-                    <p class="sahde_color">Unlock the secret to smoother, more radiant skin with Reduction Peels at Tripura Clinics. Our Reduction Peel treatments are designed to target various skin concerns, including pigmentation, uneven texture, and fine lines, leaving you with a rejuvenated and refreshed complexion. By harnessing the power of potent ingredients such as alpha hydroxy acids (AHAs) and beta hydroxy acids (BHAs), our specialized peels exfoliate the skin's surface, promoting cell turnover and revealing brighter, more youthful-looking skin underneath. Whether you're struggling with sun damage, acne scars, or dullness, Reduction Peels at Tripura Clinics offer a safe and effective solution to help you achieve your skincare goals. Say goodbye to imperfections and hello to smoother, more luminous skin with Reduction Peels at Tripura Clinics.</p>
-                    <div class="btn_flex_center">
-                        <button class="schedual_button" data-bs-toggle="modal" data-bs-target="#exampleModal"> Schedule Now!</button>
-                    </div>
-                </div>
-                <div class="col-md-6  d-flex flex-column justify-content-center order-1 order-md-2 mt-2">
-                    <img src="assets/images/reduction peel large.png" class=" doc_img_one img-fluid" alt="...">
-                </div>
-            </div>
-        </div>
-    </section>
-
-    <!-- laser brightening -->
-    <section id="Laser_Brigthening">
-        <div class="container mt-5  div_first_con">
-            <div class="row">
-                <div class="col-md-6  d-flex flex-column justify-content-center  mt-2 order-1 order-md-1">
-                    <img src="assets/images/laser brighrt large.png" class=" doc_img_one img-fluid" alt="...">
-
-                </div>
-                <div class="col-md-6  order-2 order-md-2">
-                    <h6 class="shedual_heading">Make a Schedule</h6>
-                    <h2 class="shedual_para">Laser Brightening</h2>
-                    <p class="sahde_color">Laser Brightening treatments offer a cutting-edge solution to brighten and rejuvenate the skin while targeting pigmentation concerns. Our advanced laser technology precisely targets pigmented areas, breaking down excess melanin and stimulating collagen production for brighter, more even-toned skin. Laser brightening treatments are safe, effective, and non-invasive, providing noticeable results with minimal downtime. Say goodbye to pigmentation woes and hello to a luminous complexion with our laser brightening treatments. Experience the benefits of laser technology for radiant, rejuvenated skin at our clinic.
-                    </p>
-                    <div class="btn_flex_center">
-                        <button class="schedual_button" data-bs-toggle="modal" data-bs-target="#exampleModal"> Schedule Now!</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-
-    <!-- Growth Factor Concentrate -->
-    <section id="Growth_Factor_Concentrate">
-        <div class="container mt-5  div_first_con">
-            <div class="row">
-                
-                <div class="col-md-6 order-2 order-md-1">
-                    <h6 class="shedual_heading">Make a Schedule</h6>
-                    <h2 class="shedual_para">Growth Factor Concentrate</h2>
-                    <p class="sahde_color">Growth Factor Concentrate therapy offers a versatile solution for reducing pigmentation and promoting skin renewal. By harnessing the power of growth factors, this advanced therapy stimulates cell turnover, collagen production, and skin regeneration, resulting in a more even complexion and reduced pigmentation. Growth factors help repair damaged skin cells, fade dark spots, and enhance skin tone and texture. This innovative treatment not only addresses pigmentation concerns but also rejuvenates the skin, leaving it smoother, brighter, and more youthful. Experience the transformative effects of Growth Factor Concentrate therapy for pigmentation reduction and skin renewal at our clinic.</p>
-                    <div class="btn_flex_center">
-                        <button class="schedual_button" data-bs-toggle="modal" data-bs-target="#exampleModal"> Schedule Now!</button>
-                    </div>
-                </div>
-                <div class="col-md-6  d-flex flex-column justify-content-center mt-2 order-1 order-md-2">
-                    <img src="assets/images/GFC la_cleanup.jpeg" class=" doc_img_one img-fluid" alt="...">
-                </div>
-            </div>
-        </div>
-    </section>
-
-  
-     <!-- Model  -->
-     <?php include 'modal.php' ?>
+   </section>
+    <!-- Model  -->
+    <?php include 'modal.php' ?>
 
     <!-- Scroll Up Button  -->
 <button id="scrollBtn" onclick="scrollToTop()"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-up" viewBox="0 0 16 16">
@@ -226,7 +147,6 @@
         }
     }
 </script>
-
 
 
     <!-- footer -->
@@ -357,8 +277,6 @@
     </div>
 </section>
     
-    
-
     <!-- whatsapp icon  -->
 <a href="https://api.whatsapp.com/send?phone=919493066633
 " target="_blank">
